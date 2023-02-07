7ed0b97c5b4f756a7306a4d447648fe9508a2a77
Manimaran Ramesh <124668741+manimaranrameshgithub@users.noreply.github.com>
2023-02-07 15:25:46 +0530

    Update streamlit_app.py
    Adding header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.texte('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Har-Boiled Free-Range Egg')
+
