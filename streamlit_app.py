91f43de0ccb23567f14f7ffc7446c6c3118252cf
Manimaran Ramesh <124668741+manimaranrameshgithub@users.noreply.github.com>
2023-02-07 15:26:42 +0530

    Update streamlit_app.py
    typing mistake with text

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.texte('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Har-Boiled Free-Range Egg')
 
