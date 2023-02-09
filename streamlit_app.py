ef3333a42512bff167a27aaff4f93547ba4fbc63
Manimaran Ramesh <124668741+manimaranrameshgithub@users.noreply.github.com>
2023-02-09 09:37:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ except URLError as e:
    streamlit.error()
 
 #dont run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 streamlit.header("The fruit load list contains:")
 #SNOWFLAKE-RELATED FUNCTION
